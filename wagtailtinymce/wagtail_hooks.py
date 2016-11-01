f9f17e2f6c4736ac3318748cf3c535db04fedcd0
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-11-01 12:58:21 +0200

    Replace TinyMCE with bundle build 4.4.3
    Build acquired from https://www.tinymce.com/download/custom-builds/ with full settings

--- wagtailtinymce/wagtail_hooks.py
+++ wagtailtinymce/wagtail_hooks.py
@@ -66,10 +66,10 @@ def insert_editor_js():
         '    window.tinymce.suffix = "";'
         '}}());'
         '</script>',
-        to_js_primitive(static('wagtailtinymce/js/vendor/tinymce')),
+        to_js_primitive(static('wagtailtinymce/tinymce')),
     )
     js_files = [
-        'wagtailtinymce/js/vendor/tinymce/tinymce.jquery.js',
+        'wagtailtinymce/tinymce/tinymce.min.js',
         'wagtailtinymce/js/tinymce-editor.js',
     ]
     js_includes = format_html_join(
